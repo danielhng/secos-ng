657ea1935966de4fd1c422137807c761e45a4503
Danielhng <danielhennig04@hotmail.com>
2024-12-30 21:48:31 +0100

    exam finished

--- tp_exam/README.md
+++ tp_exam/README.md
@@ -1,100 +1,27 @@
-# Examen SecOS - (c) Airbus
+** Disclaimer **
 
-L'examen consiste en la création d'un OS multi-tâches préemptif, en vous
-appuyant sur vos connaissances du cours et les mini TPs réalisés.
+Notre travail n'est malheureusement pas entierement fonctionnel. 
+Cela est du a une non réussite de notre part.
+En effet l'appel de iret dans notre clock raise une page fault. C'est malheureux et nous n'avons pas trouver le moyen de résoudre le problème, à part de le localiser probablement dans l'execution de dépilation de la stack de la tache courante. 
 
-Vous êtes libres de modifier l'intégralité du noyau si vous le jugez
-nécessaire.
+Normalement, si nous avions résolus ce problème, l'execution du programme aurait entrainé une première éxecution de la tache 1 donc d'incrémentation, puis de read ect grace a la fonction clock.
 
----
 
-## Objectifs
+** cartographie memoire **
 
-Permettre un ordonnancement préemptif de 2 tâches (T1 et T2) en ring 3 :
+base de la pgd: 0x200000
+base de la ptb: 0x201000
 
- - Le code et les données des tâches seront inclus dans le noyau à la compilation, comme nous l'avons fait dans les TPs précédents pour la fonction `userland()`.
- - Chaque tâche sera représentée par une fonction (ex. `user1()`, `user2()`) exécutant une boucle infinie.
- - La pagination doit être activée:
-   + Le noyau est identity mappé
-   + Les tâches sont identity mappées
-   + Les tâches possèdent leurs propres PGD/PTB
-   + Les tâches ont une zone de mémoire partagée :
-     - De la taille d'une page (4KB)
-     - À l'adresse physique de votre choix
-     - À des adresses virtuelles différentes
-   + Les tâches doivent avoir leur propre pile noyau (4KB)
-   + Les tâches doivent avoir leur propre pile utilisateur (4KB)
- - La tâche 1 écrit un compteur (uint32_t) dans la zone de mémoire partagée
- - La tâche 2 affiche le compteur depuis la zone de mémoire partagée
- - L'affichage dans la tâche 2 s'effectuera à l'aide d'un appel système :
-   + L'interface utilisateur :
-     - A le prototype: `void sys_counter(uint32_t *counter);`
-     - Notez bien que l'argument est une adresse virtuelle ring 3
-   + L'interface noyau :
-     - Est installée à l'interruption `0x80`
-     - Reçoit un pointeur utilisateur de type `uint32_t*`
-     - S'occupe de l'affichage du compteur (via `debug()`)
- - Le gestionnaire de l'interruption 32 (irq0 = horloge) doit vous permettre à chaque interruption de passer de la tâche 1 à la tâche 2 et inversement. Il doit également savoir détecter s'il a interrompu le noyau ou une tâche utilisateur.
- - Une fois la mémoire, les interruptions et les tâches initialisées, le noyau doit activer les interruptions matérielles et effectuer une boucle infinie.
+tache 1: pgd en 0x202000
+         page kernel identity mappee en 0x203000 adresses 0x0 a 0x0400
+         page user privee identity mappee en 0x204000 adresses 0x400 a 0x800
+         page user partagee en 0x205000 adresses 0x800 a 0xc00
 
- ---
+tache 2: pgd en 0x206000
+         page kernel identity mappee en 0x207000 adresses 0x0 a 0x400
+         page user privee identity mappee en 0x209000 adresses 0x800 a 0xc00
+         page user partagee en 0x208000 adresses 0x400 a 0x800
 
-## Astuces
-
- - Faites vous une cartographie mémoire complète pour vous y retrouver :
-   . Adresses physiques noyau
-   . Adresses physiques utilisateurs
-   . Adresses virtuelles utilisateurs
-
- - Vous pouvez modifier le linker script afin de situer votre code utilisateur
-   à des adresses physiques pratiques (alignées sur une entrée de PDE par
-   exemple (ex 4MB, 8MB). Ceci vous aidera à configurer le PGD des tâches
-   ring 3. Pour cela il faut :
-   + Créer une section ".user" dans linker.lds
-   + Préfixer vos fonctions `user1()` et `user2()` avec l'attribut de section GCC `__attribute__((section(".user")))`.
-   + Regardez "include/mbi.h" et "core/start.c" pour vous inspirer de la manière dont les en-têtes multiboot sont déclarées.
-   + Vous pouvez également créer une section pour les données user. Cette section pourrait contenir les piles user, la page de mémoire partagée par exemple.
-
- - Faites attention aux droits des PDEs/PTEs ... ils sont combinés !
-
- - Les piles avancent à l'envers ! On part des adresses hautes.
-
- - La première exécution d'une tâche est un peu délicate à mettre en oeuvre.
-   Pensez bien au chemin d'exécution (retours successifs d'appels de
-   fonctions) qu'elle va devoir parcourir avant d'atteindre
-   l'instruction "iret" responsable de la bascule en ring 3. Il s'agît de
-   correctement initialiser le contexte d'interruption initial de la tâche,
-   au sommet de sa pile noyau.
-
- ---
-
-## À rendre
-
-J'attends un patch qui compile et me permet de tester votre travail. Je vous
-conseille de "cloner" à nouveau le repo git de secos et de commencer l'examen
-depuis cette base saine dans une nouvelle branche (et non pas depuis votre
-working directory utilisé pour les mini TPs).
-
-Note : Me partager, dans le format de votre choix (.md, .jpg, .pdf, etc.) une
-documentation succinte de vos choix de cartographie mémoire serait un plus.
-
-Un exemple de workflow:
-```bash
-$ git clone https://github.com/agantet/secos-ng
-$ cd secos
-$ git checkout -b exam
-
-$ emacs ... gcc ... rage !
-$ emacs ... gcc ... fear !
-$ emacs ... gcc ... rulz :)
-
-$ git add <file>
-$ git commit -m "exam finished"
-$ git format-patch $(git merge-base --fork-point master)..exam
-$ tar czf nom.prenom.tar.gz *.patch
-```
-
-Ceci vous génère une archive compressée contenant vos modifications, que vous
-pouvez m'envoyer par mail.
+adresse idt: 0x30dfa0
 
 
--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -2,7 +2,12 @@
 #include <debug.h>
 #include <segmem.h>
 #include <pagemem.h>
+#include <string.h>
 #include <cr.h>
+#include <intr.h>
+#include <info.h>
+#include <asm.h>
+#include <io.h>
 
 // Segmentation
 
@@ -125,8 +130,8 @@ void config_pgd()
 	 	pg_set_entry(&pgd_task_2_ptb_0[i], PG_KRN|PG_RW, i);
 
 		// identity map de chaque premiere page des taches
-		pg_set_entry(&pgd_task_1_ptb_1[i], PG_USR|PG_RW, 1024 + i); // de 0x400000 a 0x800000
-	 	pg_set_entry(&pgd_task_2_ptb_1[i], PG_USR|PG_RW, 2* 1024 + i); // de 0x800000 a 0xc00000
+		pg_set_entry(&pgd_task_1_ptb_1[i], PG_USR|PG_RW, 1024 + i); // de 0x400 a 0x800
+	 	pg_set_entry(&pgd_task_2_ptb_1[i], PG_USR|PG_RW, 2* 1024 + i); // de 0x800 a 0xc00
 	}
 
 
@@ -146,8 +151,8 @@ void config_pgd()
 	pg_set_entry(&pgd_task_2[2], PG_USR|PG_RW, page_get_nr(pgd_task_2_ptb_1)); // set page table at [2] in order to identity map task space
 
 	// entree de la page partagee dans les pgd 
-	pg_set_entry(&pgd_task_2[1], PG_USR|PG_RW, page_get_nr(pgd_task_2_ptb_2)); // 0x400000 pour tache 2
-	pg_set_entry(&pgd_task_1[2], PG_USR|PG_RW, page_get_nr(pgd_task_1_ptb_2)); // 0x800000 pour tache 1
+	pg_set_entry(&pgd_task_2[1], PG_USR|PG_RW, page_get_nr(pgd_task_2_ptb_2)); // 0x400 pour tache 2
+	pg_set_entry(&pgd_task_1[2], PG_USR|PG_RW, page_get_nr(pgd_task_1_ptb_2)); // 0x800 pour tache 1
 
 
 	set_cr3((uint32_t)pgd);
@@ -161,13 +166,230 @@ void config_pgd()
 
 // Taches
 
-void tache1() {
-	uint32_t *compteur = 0xFFFFFFFF;
+struct tache {
+    uint32_t stack;      
+    pde32_t *pgd;         
+	void *function;
+	int_ctx_t context;
+	bool_t saved_ctx;
+	uint32_t int_stack;
+}; 
+struct tache tasks[2];
 
+#define STACK_SIZE 4096
+uint32_t t1_stack[STACK_SIZE] __attribute__((aligned(4), section(".t1_stack")));
+uint32_t t2_stack[STACK_SIZE] __attribute__((aligned(4), section(".t2_stack")));
+
+uint32_t  t1_stack_base = (uint32_t)&t1_stack[STACK_SIZE-1];
+uint32_t  t2_stack_base = (uint32_t)&t2_stack[STACK_SIZE-1];
+
+__attribute__((section(".user1")))
+void tache1() {
+	uint32_t *compteur = (uint32_t*)0x800;
+	*compteur = 0;
+		*compteur += 1;
+		debug("tache 1 oui \n");
+}
+
+__attribute__((section(".user2")))
+void sys_counter(uint32_t *counter) {
+    while (true)
+	{
+		asm volatile (
+        "movl %0, %%eax\n" // Numéro d'interruption pour l'appel système
+        "int $0x80\n"      // Appel de l'interruption
+        :
+        : "r"(counter)
+        : "eax"
+    );
+	debug("sys counter oui\n");
+	}
+	
+}
+
+__attribute__((section(".user2")))
+void tache2() {
+	while (true)
+	{
+		uint32_t *shared_counter = (uint32_t*)0x400;
+		sys_counter(shared_counter);
+		debug("tache 2 oui \n");
+	}
+}
+
+void syscall_isr() {
+    asm volatile(
+        "cli\n"
+        "leave\n"
+        "pusha\n"
+        "mov %esp, %eax\n"
+        "call syscall_handler\n"
+        "popa\n"
+        "sti\n"
+        "iret\n"
+    );
+	debug("syscall isr oui\n");
+}
+
+void syscall_handler(int_ctx_t *ctx) {
+    uint32_t eax = ctx->gpr.eax.raw;
+    uint32_t *counter = (uint32_t*)eax;
+    debug("Counter value: %u\n", *counter);
+}
+
+void irq_generic_handler() {
+    outb(0x20, 0x20); 
+    asm volatile (
+	  	"cli                \n"
+      	"leave ; pusha        \n"
+      	"mov %esp, %eax      \n"
+      	"call clock \n"
+      	"popa ;    \n"
+		"sti ;  \n"
+		"iret"
+    );
+}
+
+uint32_t curr_task = 0;
+
+void __regparm__(1) clock(int_ctx_t *context) {
+	
+	debug("entre dans clock\n");
+	if((int)curr_task!=-1)   // si on  vient  du noyau, on ne sauvegarde pas le contexte
+	{
+		memcpy(&tasks[curr_task].context,context,sizeof(int_ctx_t));
+		tasks[curr_task].saved_ctx=true;
+	}
+
+ 	curr_task = (curr_task + 1) % 2;
+
+	
+
+	set_ds(d3_sel);
+	set_es(d3_sel);
+	set_fs(d3_sel);
+	set_gs(d3_sel);
+	TSS.s0.esp = tasks[curr_task].int_stack;
+    set_cr3(tasks[curr_task].pgd);
+
+	curr_task = curr_task;
+
+   if (tasks[curr_task].saved_ctx)
+   {
+		memcpy(context, &tasks[curr_task].context, sizeof(int_ctx_t));
+   }
+   else
+   {
+		asm volatile(
+			//"sti \n"
+			"push %0 \n" // esp
+			"push %1 \n" // ss
+			"pushf   \n" // eflags
+			"push %2 \n" // cs
+			"push %3 \n" // eip
+			"sti \n"
+			"iret\n"
+			::"i"(d3_sel),
+			"m"(tasks[curr_task].stack),
+			"i"(c3_sel),
+			"r"(tasks[curr_task].function));    //(tasks[curr_task].function)));
+   }
+
+	if (curr_task == 0) {
+		debug("!!Adding 1 to counter!!\n");
+	} else if (curr_task == 1)	{
+		debug("!!Reading counter!!\n");
+	}
+}
+
+
+
+void config_idt() {
+	idt_reg_t idtr;
+	get_idtr(idtr);
+	debug("Adresse de l'IDT: 0x%lx\n", idtr.addr);
+
+	debug("Adresse de syscall isr: 0x%x\n", (unsigned int) &syscall_isr);
+
+	int_desc_t *desc;
+	desc = &idtr.desc[0x80];
+	short int off_1 = (int) &syscall_isr;
+	short int off_2 = (int) &syscall_isr >> 16;
+	debug("valeur de off1: %x, valeur de off 2: %x\n", off_1, off_2);
+	desc->offset_1 = off_1;
+	desc->offset_2 = off_2;
+	desc->dpl = 3;
+
+	desc = &idtr.desc[32];
+	off_1 = (int) &irq_generic_handler;
+	off_2 = (int) &irq_generic_handler >> 16;
+	desc->offset_1 = off_1;
+	desc->offset_2 = off_2;
+	desc->dpl = 0;
+
+}
+
+void print_gdt_content(gdt_reg_t gdtr_ptr) {
+    seg_desc_t* gdt_ptr;
+    gdt_ptr = (seg_desc_t*)(gdtr_ptr.addr);
+    int i=0;
+    while ((uint32_t)gdt_ptr < ((gdtr_ptr.addr) + gdtr_ptr.limit)) {
+        uint32_t start = gdt_ptr->base_3<<24 | gdt_ptr->base_2<<16 | gdt_ptr->base_1;
+        uint32_t end;
+        if (gdt_ptr->g) {
+            end = start + ( (gdt_ptr->limit_2<<16 | gdt_ptr->limit_1) <<12) + 4095;
+        } else {
+            end = start + (gdt_ptr->limit_2<<16 | gdt_ptr->limit_1);
+        }
+        debug("%d ", i);
+        debug("[0x%x ", start);
+        debug("- 0x%x] ", end);
+        debug("seg_t: 0x%x ", gdt_ptr->type);
+        debug("desc_t: %d ", gdt_ptr->s);
+        debug("priv: %d ", gdt_ptr->dpl);
+        debug("present: %d ", gdt_ptr->p);
+        debug("avl: %d ", gdt_ptr->avl);
+        debug("longmode: %d ", gdt_ptr->l);
+        debug("default: %d ", gdt_ptr->d);
+        debug("gran: %d ", gdt_ptr->g);
+        debug("\n");
+        gdt_ptr++;
+        i++;
+    }
+}
+
+void config_task(struct tache *task, void *function, uint32_t stack,uint32_t int_stack, pde32_t *pgd) {
+    task->function = function;
+    task->stack = stack;
+    task->pgd = pgd;
+	task->int_stack=int_stack;
+    memset(&task->context, 0, sizeof(int_ctx_t));
+	task->saved_ctx = false;
 }
 
 void tp() {
 	// TODO
 	config_seg();
 	config_pgd();
+	config_idt();
+	gdt_reg_t gdt;
+	get_gdtr(gdt);
+	print_gdt_content(gdt);
+
+
+	config_task(&tasks[0], tache1, t1_stack_base,0x303000+STACK_SIZE ,pgd_task_1);
+    config_task(&tasks[1], tache2, t2_stack_base,0x304000+STACK_SIZE ,pgd_task_2);
+
+
+	set_ds(d3_sel);
+    set_es(d3_sel);
+    set_fs(d3_sel);
+    set_gs(d3_sel);
+	TSS.s0.esp = tasks[0].int_stack;
+	set_cr3((uint32_t)tasks[0].pgd);
+
+	force_interrupts_on();
+	
+	while(1);
 }
+
